f1108579a5a2543810a70f4aa2b240ad14d323e1
Casinoapk <stelelena7@gmail.com>
2025-10-12 13:52:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,54 +1,39 @@
-const CACHE_NAME = 'cache-v1';
-
-const baseUrlsToCache = [
-  '/icon/index.html',
+const CACHE_NAME = 'my-cache-v1';
+const DYNAMIC_CACHE = 'dynamic-cache-v1';
+const urlsToCache = [
+  '/styles.min.css',
+  '/',
+  '/index.html',
+  '/script.js',
+  '/favicon.ico',
+  '/favicon.svg',
   '/icon/logo192.png',
-  '/icon/logo512.png'
+  '/icon/logo512.png',
+  '/404.html'
 ];
 
-async function getAllFilesInDirectory(directory) {
-  const response = await fetch(directory);
-  const html = await response.text();
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-  const links = Array.from(doc.querySelectorAll('a[href]'))
-    .map(a => a.getAttribute('href'))
-    .filter(href => !href.endsWith('/') && !href.includes('?') && !href.startsWith('http'));
-  return links.map(file => `${directory}${file}`);
-}
-
-async function getUrlsToCache() {
-  let urls = [...baseUrlsToCache];
-
-  const subdirectories = [
-    'https://vodka-kaz.ru/index.html'
-  ];
-
-  for (const dir of subdirectories) {
-    try {
-      const files = await getAllFilesInDirectory(dir);
-      urls = [...urls, ...files];
-    } catch (e) {
-      console.warn(`Не удалось получить файлы из ${dir}:`, e);
-    }
-  }
-
-  return urls;
-}
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    getUrlsToCache().then((urlsToCache) => {
-      return caches.open(CACHE_NAME)
-        .then((cache) => cache.addAll(urlsToCache));
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then(response => response || fetch(event.request))
   );
 });
-
